623278f5975d347a5142af3848e62c84b97fefdc
FernandoFdeS <fernandofdes@hotmail.com>
2023-08-31 15:20:15 -0300

    Refatora o codigo

---
+++ Disciplina.py
@@ -0,0 +1,7 @@
+class Disciplina:
+    def __init__(self,curso,alunos,horarios,salasPreferenciais):
+        self.curso = curso
+        self.alunos = alunos
+        self.horarios = horarios
+        self.salasPreferenciais = salasPreferenciais
+    pass
