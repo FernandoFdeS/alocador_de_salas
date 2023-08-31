9f8a1565b3f0e1e4b206462fcfefa43d00cd2610
FernandoFdeS <fernandofdes@hotmail.com>
2023-08-31 10:46:07 -0300

    Pequenas alteracoes no extrai_salas

--- extrai_salas.py
+++ extrai_salas.py
@@ -1,14 +1,14 @@
 import pandas as pd
 from Sala import Sala
 
-dados = pd.read_csv("./dados/salas.csv", encoding="UTF-8" , sep = ",", usecols=["BLOCO","CADEIRAS"])
+dados = pd.read_csv("./dados/salas.csv", encoding="UTF-8" , sep = ",", usecols=["SALAS","CADEIRAS"])
 blocoAtual = ""
 numeroSala = ""
 vet_salas=[]
 nome_salas=[]
 
 for index, row in dados.iterrows():
-    sala = str(row["BLOCO"])
+    sala = str(row["SALAS"])
 
     if(sala.find("BLOCO")==0):
         blocoAtual=sala.split(" ")[1] # Pegando o Bloco da Sala
@@ -18,7 +18,7 @@ for index, row in dados.iterrows():
     cadeiras = row["CADEIRAS"]
     if(pd.isna(sala) or pd.isna(cadeiras)):
         continue
-    # Salas especiais ("Espaços Ajeitados") não tem número de cadeiras definidas no csv, então são ignoradas.
+    # Salas especiais ("Espaços Ajeitados") originalmente não tem número de cadeiras definidas no csv, então são ignoradas.
     cadeiras=int(cadeiras)
     vet_salas.append(Sala(cadeiras))
     nome_salas.append((numeroSala+"-"+blocoAtual))
@@ -27,3 +27,5 @@ salas = dict()
 for idx,sala in enumerate(range(len(vet_salas))):
     salas[nome_salas[idx]]=vet_salas[idx]
 
+# for sala in salas:
+#     print (sala, salas[sala].capacidade)
